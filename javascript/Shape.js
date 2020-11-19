939d172f6dbc8b8c25242f16d0498750a01fdf11
Ibbs620 <58309356+Ibbs620@users.noreply.github.com>
2020-11-19 14:28:56 -0500

    Added tetromino rotation and hard drop

--- javascript/Shape.js
+++ javascript/Shape.js
@@ -10,48 +10,55 @@ class Shape{
                 this.block2 = new Block(4, 1, this.color);
                 this.block3 = new Block(5, 1, this.color);
                 this.block4 = new Block(6, 1, this.color);
+                this.pivot = {x: 4.5, y: 1.5};
                 break;
             case('o'):
                 this.color = 'yellow'
-                this.block1 = new Block(5, 0, this.color);
+                this.block1 = new Block(4, 1, this.color);
                 this.block2 = new Block(4, 0, this.color);
                 this.block3 = new Block(5, 1, this.color);
-                this.block4 = new Block(4, 1, this.color);
+                this.block4 = new Block(5, 0, this.color);
+                this.pivot = {x: 4.5, y: 0.5};
                 break;
             case('t'):
                 this.color = 'purple'
                 this.block1 = new Block(3, 1, this.color);
-                this.block2 = new Block(4, 1, this.color);
+                this.block2 = new Block(4, 0, this.color);
                 this.block3 = new Block(5, 1, this.color);
-                this.block4 = new Block(4, 0, this.color);
+                this.block4 = new Block(4, 1, this.color);
+                this.pivot = {x: 4, y: 1};
                 break;
             case('s'):
                 this.color = 'lime'
                 this.block1 = new Block(3, 1, this.color);
-                this.block2 = new Block(4, 1, this.color);
+                this.block2 = new Block(4, 0, this.color);
                 this.block3 = new Block(5, 0, this.color);
-                this.block4 = new Block(4, 0, this.color);
+                this.block4 = new Block(4, 1, this.color);
+                this.pivot = {x: 4, y: 1};
                 break;
             case('z'):
                 this.color = 'red'
                 this.block1 = new Block(5, 1, this.color);
-                this.block2 = new Block(4, 1, this.color);
+                this.block2 = new Block(3, 0, this.color);
                 this.block3 = new Block(4, 0, this.color);
-                this.block4 = new Block(3, 0, this.color);
+                this.block4 = new Block(4, 1, this.color);
+                this.pivot = {x: 4, y: 1};
                 break;
             case('j'):
                 this.color = 'orange'
                 this.block1 = new Block(3, 0, this.color);
                 this.block2 = new Block(3, 1, this.color);
-                this.block3 = new Block(4, 1, this.color);
-                this.block4 = new Block(5, 1, this.color);
+                this.block3 = new Block(5, 1, this.color);
+                this.block4 = new Block(4, 1, this.color);
+                this.pivot = {x: 4, y: 1};
                 break;
             case('l'):
                 this.color = 'blue'
                 this.block1 = new Block(5, 0, this.color);
                 this.block2 = new Block(3, 1, this.color);
-                this.block3 = new Block(4, 1, this.color);
-                this.block4 = new Block(5, 1, this.color);
+                this.block3 = new Block(5, 1, this.color);
+                this.block4 = new Block(4, 1, this.color);
+                this.pivot = {x: 4, y: 1};
                 break;
         }
     }
@@ -62,6 +69,7 @@ class Shape{
             this.block2.moveDown();
             this.block3.moveDown();
             this.block4.moveDown();
+            this.pivot.y++;
         } else {
             this.dead = true;
         }
@@ -73,6 +81,7 @@ class Shape{
             this.block2.moveLeft();
             this.block3.moveLeft();
             this.block4.moveLeft();
+            this.pivot.x--;
         }
     }
 
@@ -82,7 +91,17 @@ class Shape{
             this.block2.moveRight();
             this.block3.moveRight();
             this.block4.moveRight();
+            this.pivot.x++;
         }
+    }
+
+    hardDrop(){
+        while(this.canMoveDown()) this.moveDown();
+        this.dead = true;
+    }
+
+    softDrop(){
+
     }
 
     canMoveDown(){
@@ -116,6 +135,44 @@ class Shape{
         && this.blockMatrix.deadBlocks[this.block2.y][this.block2.x - 1] == 0
         && this.blockMatrix.deadBlocks[this.block3.y][this.block3.x - 1] == 0
         && this.blockMatrix.deadBlocks[this.block4.y][this.block4.x - 1] == 0;
+    }
+
+    rotateShape(direction){
+        let rotatedShape = this.createRotatedShape(direction);
+        if(!this.checkShapeOverlap(rotatedShape)) {
+            this.block1 = rotatedShape.block1;
+            this.block2 = rotatedShape.block2;
+            this.block3 = rotatedShape.block3;
+            this.block4 = rotatedShape.block4;
+        }
+    }
+
+    createRotatedShape(direction){
+        let rotatedShape = new Shape(this.shapeID);
+        if(this.shapeID == 'i'){
+            
+        }
+        if (this.shapeID == 'o') return;
+        rotatedShape.block1.x = (this.block1.y - this.pivot.y) * direction  + this.pivot.x;
+        rotatedShape.block1.y = (this.block1.x - this.pivot.x) * -direction  + this.pivot.y;
+        rotatedShape.block2.x = (this.block2.y - this.pivot.y) * direction  + this.pivot.x;
+        rotatedShape.block2.y = (this.block2.x - this.pivot.x) * -direction  + this.pivot.y;
+        rotatedShape.block3.x = (this.block3.y - this.pivot.y) * direction  + this.pivot.x;
+        rotatedShape.block3.y = (this.block3.x - this.pivot.x) * -direction  + this.pivot.y;
+        rotatedShape.block4.x = (this.block4.y - this.pivot.y) * direction  + this.pivot.x;
+        rotatedShape.block4.y = (this.block4.x - this.pivot.x) * -direction  + this.pivot.y;
+        return rotatedShape;
+    }
+
+    checkShapeOverlap(shape){
+        if(shape.block1.x < 0 || shape.block1.x > 9 || shape.block1.y > 23 ||
+           shape.block2.x < 0 || shape.block2.x > 9 || shape.block2.y > 23 ||
+           shape.block3.x < 0 || shape.block3.x > 9 || shape.block3.y > 23 ||
+           shape.block4.x < 0 || shape.block4.x > 9 || shape.block4.y > 23) return true;
+        return this.blockMatrix.deadBlocks[shape.block1.y][shape.block1.x] == 1
+        || this.blockMatrix.deadBlocks[shape.block2.y][shape.block2.x] == 1
+        || this.blockMatrix.deadBlocks[shape.block3.y][shape.block3.x] == 1
+        || this.blockMatrix.deadBlocks[shape.block4.y][shape.block4.x] == 1;
     }
 
     draw(){
